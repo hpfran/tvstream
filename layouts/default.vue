fb75fac319932de1c9fe8d78b56b12ed93602fe5
hpfran <hpfran@gmail.com>
2021-03-26 14:02:49 +0100

    commit prototipo

---
+++ layouts/default.vue
@@ -0,0 +1,236 @@
+<template>
+  <div class="wrapper" :class="{ collapsed: collapsedSidebar }">
+    <div class="box header"><span class="logo">tvstream</span></div>
+    <div class="box sidebar">
+      <h3>categorías</h3>
+      <div class="categorias-tags">
+        <span class="tag selected">política</span>
+        <span class="tag">deporte</span>
+        <span class="tag">gaming</span>
+        <span class="tag">forex</span>
+        <span class="tag">inversión</span>
+        <span class="tag">televisión</span>
+      </div>
+    </div>
+
+    <Nuxt class="box content" />
+
+    <div class="box footer">&copy; tvstream {{ new Date().getFullYear() }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      collapsedSidebar: false,
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+:root {
+  --padding: 4rem;
+  --border: rgb(0, 0, 0, 0.2);
+}
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+.heading {
+  line-height: 2;
+  font-weight: 900;
+}
+
+.section-container {
+  padding: 0 1rem 1rem 0;
+  a {
+    color: var(--main-color);
+  }
+}
+
+.schedule-container {
+  overflow: auto;
+}
+
+.schedule-stage__wrapper {
+  display: inline-block;
+}
+
+.schedule-stage {
+  display: grid;
+  grid-auto-columns: max-content;
+  grid-auto-flow: column;
+  grid-gap: 2rem;
+  padding-bottom: 5rem;
+  padding-top: 2rem;
+  min-width: 100vw;
+
+  &::after {
+    content: '';
+    width: var(--padding);
+  }
+}
+
+.divider {
+  position: sticky;
+  left: 0;
+  border: none;
+  border-bottom: 1px solid var(--border);
+}
+
+.schedule-stage:nth-child(1) > .schedule-stage__title > h3 {
+  background: rgb(153, 16, 135);
+  background: linear-gradient(to right, rgb(156, 41, 141), rgb(161, 10, 141));
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.schedule-stage:nth-child(3) > .schedule-stage__title > h3 {
+  background: rgb(51, 139, 23);
+  background: linear-gradient(to right, #1d976c, #93f9b9);
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.schedule-stage__title {
+  position: sticky;
+  left: 0;
+  padding-right: 3rem;
+  height: 100%;
+  padding-left: 1rem;
+  background: linear-gradient(to right, #efefef 25%, hsla(0, 0%, 0%, 0));
+  & > h3 {
+    font-weight: 900;
+  }
+}
+
+.schedule-stage_guide-container {
+  display: grid;
+  grid-auto-flow: column;
+  grid-auto-columns: 17rem;
+  grid-gap: 1.5rem;
+}
+
+.guide-slot {
+  color: #333;
+  & > p {
+    margin-bottom: 1rem;
+    font-weight: 700;
+    color: #666;
+    text-align: center;
+  }
+}
+
+.guide-slot_card {
+  background: #fafafa;
+  /*border: 1px solid var(--border);*/
+  box-shadow: 1px 1px 1px rgba(43, 59, 93, 0.3);
+  border-radius: 0.8rem;
+  font-size: 1rem;
+  margin-bottom: 1rem;
+  padding: 1rem;
+  cursor: pointer;
+
+  &.selected {
+    background: #eedefe;
+  }
+  &.vip {
+    background-color: var(--accent-color);
+    color: white;
+  }
+  &:hover {
+    border-color: hsl(0, 0%, 60%);
+  }
+
+  & > h4 {
+    margin-right: 1rem;
+    font-size: 0.9rem;
+  }
+}
+
+.author-wrapper {
+  display: flex;
+  align-items: center;
+  margin-top: 0.5rem;
+  & figcaption {
+    font-size: 1rem;
+  }
+  & > picture {
+    display: inline-block;
+    inline-size: 2rem;
+    block-size: 2rem;
+    border-radius: 50%;
+    overflow: hidden;
+    margin-right: 1rem;
+    border: 1px solid var(--border);
+    & > img {
+      inline-size: 100%;
+      block-size: 100%;
+      object-fit: cover;
+    }
+  }
+}
+.categorias-tags {
+  display: flex;
+  flex-wrap: wrap;
+  & .tag {
+    padding: 0.35rem 0.8rem;
+    margin: 0.5rem;
+    font-weight: 500;
+    border-radius: 0.5rem;
+    background-color: #eee;
+    text-overflow: ellipsis;
+    &.selected {
+      background-color: var(--main-color);
+      color: white;
+    }
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,270 @@
+<template>
+  <div class="container">
+    <section class="section-container">
+      <h1 class="heading">Schedule</h1>
+      <p>Próximos streamings</p>
+    </section>
+
+    <hr class="divider" />
+
+    <div class="schedule-container">
+      <div class="schedule-stage__wrapper">
+        <section class="schedule-stage">
+          <div class="schedule-stage__title">
+            <h3>{{ hoy | weekDay }}</h3>
+          </div>
+          <div class="schedule-stage_guide-container">
+            <div class="guide-slot">
+              <p class="time-slot">8:00am - 9:00am</p>
+              <div class="guide-slot_card selected">
+                <h4>CSR, SSR, SSG, ISR and OMG.WTF?BBQ!</h4>
+                <figure class="author-wrapper">
+                  <picture>
+                    <img
+                      src="https://randomuser.me/api/portraits/lego/6.jpg"
+                      alt=""
+                    />
+                  </picture>
+                  <figcaption>Ahmad Awais</figcaption>
+                </figure>
+              </div>
+            </div>
+            <div class="guide-slot">
+              <p>9:00am - 10:00am</p>
+              <div class="guide-slot_card">
+                <h4>Changing Lanes with Lyft</h4>
+                <figure class="author-wrapper">
+                  <picture>
+                    <img
+                      src="https://randomuser.me/api/portraits/lego/2.jpg"
+                      alt=""
+                    />
+                  </picture>
+                  <figcaption>Joshua Callender</figcaption>
+                </figure>
+              </div>
+              <div class="guide-slot_card">
+                <h4>Magic Authentication - The Missing LEGO Piece</h4>
+                <figure class="author-wrapper">
+                  <picture>
+                    <img
+                      src="https://randomuser.me/api/portraits/lego/1.jpg"
+                      alt=""
+                    />
+                  </picture>
+                  <figcaption>Sean Li</figcaption>
+                </figure>
+              </div>
+            </div>
+            <div class="guide-slot">
+              <p>10:00am - 11:00am</p>
+              <div class="guide-slot_card">
+                <h4>
+                  Introducing: Edge Slice Rerendering. Performant A/B Testing,
+                  Personalization, and more.
+                </h4>
+                <figure class="author-wrapper">
+                  <picture>
+                    <img
+                      src="https://randomuser.me/api/portraits/lego/3.jpg"
+                      alt=""
+                    />
+                  </picture>
+                  <figcaption>Jay Phelps</figcaption>
+                </figure>
+              </div>
+            </div>
+            <div class="guide-slot">
+              <p>11:00am - 12:00pm</p>
+              <div class="guide-slot_card selected">
+                <h4>Everything Is a CMS</h4>
+                <figure class="author-wrapper">
+                  <picture>
+                    <img
+                      src="https://randomuser.me/api/portraits/lego/5.jpg"
+                      alt=""
+                    />
+                  </picture>
+                  <figcaption>Sean C Davis</figcaption>
+                </figure>
+              </div>
+            </div>
+            <div class="guide-slot">
+              <p>12:00pm - 1:00pm</p>
+              <div class="guide-slot_card">
+                <h4>Responsive Rendering with Next.js</h4>
+                <figure class="author-wrapper">
+                  <picture>
+                    <img
+                      src="https://randomuser.me/api/portraits/lego/8.jpg"
+                      alt=""
+                    />
+                  </picture>
+                  <figcaption>Armando Gonzalez</figcaption>
+                </figure>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <hr class="divider" />
+
+        <section class="schedule-stage">
+          <div class="schedule-stage__title">
+            <h3>{{ hoy.addDays(1) | weekDay }}</h3>
+          </div>
+          <div class="schedule-stage_guide-container">
+            <div class="guide-slot">
+              <p>8:00am - 9:00am</p>
+              <div class="guide-slot_card">
+                <h4>Changing Lanes with Lyft</h4>
+                <figure class="author-wrapper">
+                  <picture>
+                    <img
+                      src="https://randomuser.me/api/portraits/lego/2.jpg"
+                      alt=""
+                    />
+                  </picture>
+                  <figcaption>Joshua Callender</figcaption>
+                </figure>
+              </div>
+              <div class="guide-slot_card selected">
+                <h4>Magic Authentication - The Missing LEGO Piece</h4>
+                <figure class="author-wrapper">
+                  <picture>
+                    <img
+                      src="https://randomuser.me/api/portraits/lego/1.jpg"
+                      alt=""
+                    />
+                  </picture>
+                  <figcaption>Sean Li</figcaption>
+                </figure>
+              </div>
+            </div>
+            <div class="guide-slot">
+              <p>9:00am - 10:00am</p>
+              <div class="guide-slot_card vip">
+                <h4>CSR, SSR, SSG, ISR and OMG.WTF?BBQ!</h4>
+                <figure class="author-wrapper">
+                  <picture>
+                    <img
+                      src="https://randomuser.me/api/portraits/lego/6.jpg"
+                      alt=""
+                    />
+                  </picture>
+                  <figcaption>Ahmad Awais</figcaption>
+                </figure>
+              </div>
+            </div>
+            <div class="guide-slot">
+              <p>10:00am - 11:00am</p>
+              <div class="guide-slot_card">
+                <h4>Responsive Rendering with Next.js</h4>
+                <figure class="author-wrapper">
+                  <picture>
+                    <img
+                      src="https://randomuser.me/api/portraits/lego/8.jpg"
+                      alt=""
+                    />
+                  </picture>
+                  <figcaption>Armando Gonzalez</figcaption>
+                </figure>
+              </div>
+            </div>
+            <div class="guide-slot">
+              <p>11:00am - 12:00pm</p>
+              <div class="guide-slot_card">
+                <h4>Everything Is a CMS</h4>
+                <figure class="author-wrapper">
+                  <picture>
+                    <img
+                      src="https://randomuser.me/api/portraits/lego/5.jpg"
+                      alt=""
+                    />
+                  </picture>
+                  <figcaption>Sean C Davis</figcaption>
+                </figure>
+              </div>
+            </div>
+            <div class="guide-slot">
+              <p>12:00pm - 1:00pm</p>
+              <div class="guide-slot_card">
+                <h4>
+                  Introducing: Edge Slice Rerendering. Performant A/B Testing,
+                  Personalization, and more.
+                </h4>
+                <figure class="author-wrapper">
+                  <picture>
+                    <img
+                      src="https://randomuser.me/api/portraits/lego/3.jpg"
+                      alt=""
+                    />
+                  </picture>
+                  <figcaption>Jay Phelps</figcaption>
+                </figure>
+              </div>
+            </div>
+          </div>
+        </section>
+        <hr class="divider" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+// eslint-disable-next-line no-extend-native
+Date.prototype.addDays = function (d) {
+  return new Date(this.valueOf() + 864e5 * d)
+}
+
+export default {
+  filters: {
+    weekDay(date, locale = 'es') {
+      moment.locale(locale)
+      return moment(new Date(date)).format('dddd')
+    },
+  },
+  data() {
+    return {
+      hoy: new Date(),
+    }
+  },
+}
+</script>
+
+<style>
+.container {
+  /* margin: 0 auto; */
+  min-height: 100vh;
+  /*
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  */
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
